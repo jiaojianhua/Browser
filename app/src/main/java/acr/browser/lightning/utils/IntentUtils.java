af761fc07e4be8155c115fd581b5b6147045ea26
Jiao Jianhua <16467515@qq.com>
2017-06-30 15:58:41 +0800

    FIX: swallow invalid network url in redict

--- app/src/main/java/acr/browser/lightning/utils/IntentUtils.java
+++ app/src/main/java/acr/browser/lightning/utils/IntentUtils.java
@@ -10,6 +10,7 @@ import android.os.Build;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.util.Log;
+import android.webkit.URLUtil;
 import android.webkit.WebView;
 
 import java.net.URISyntaxException;
@@ -59,7 +60,7 @@ public class IntentUtils {
                 mActivity.startActivity(intent);
                 return true;
             } else {
-                return false;
+                return URLUtil.isNetworkUrl(url) ? false : true;
             }
         }
         if (tab != null) {
